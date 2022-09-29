1ed2ff758b227fe8cbbf36da092190faf0eb9518
Vincent VAUBAN <v.vauban-ext@malakoffmederic-humanis.com>
2022-09-29 09:41:10 +0200

    init

---
+++ test.js
@@ -0,0 +1,12 @@
+
+// console.log(['x','x','x','x'].map(x=>'y'))
+
+//console.log(['x','y','x','x'].filter(x=>x==='x'))
+
+//console.log(['x','x','x','x'].fill('y',1))
+
+//console.log(['x', 'y', 'y', 'x'].find(x =>x=== 'x'))
+
+//console.log(['x', 'y', 'x', 'x'].findIndex(y =>y=== 'y'))
+
+console.log(['x', 'x', 'y', 'y'].every(y =>y=== 'y'))
